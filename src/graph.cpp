774c0c309ff0fcf5c7e25966a8ea369c026e38ec
Raúl Bocanegra Algarra <raul.bocanegra.algarra@gmail.com>
2017-08-12 12:29:42 +0200

    Implemented some helper functions

--- src/graph.cpp
+++ src/graph.cpp
@@ -2,6 +2,8 @@
 #include "common/common.h"
 #include <iostream>
 #include <algorithm>
+#include <numeric>
+#include <iterator>
 
 using namespace algorithms;
 using namespace algorithms::graphs;
@@ -18,6 +20,29 @@ namespace
 		return constants::repel / sqrt(pow((c1.first - c0.first), 2) + pow((c1.second - c0.second), 2));
 	}
 }
+
+NodeForce algorithms::graphs::operator-(const NodeForce & left, const NodeForce & right)
+{
+	return NodeForce{ left.first - right.first, left.second - right.second };
+}
+
+NodeForce algorithms::graphs::operator+(const NodeForce & left, const NodeForce & right)
+{
+	return NodeForce{ left.first + right.first, left.second + right.second };
+}
+
+NodeForce& algorithms::graphs::operator+=(NodeForce & left, const NodeForce & right)
+{
+	left = left + right;
+	return left;
+}
+
+NodeForce& algorithms::graphs::operator-=(NodeForce & left, const NodeForce & right)
+{
+	left = left - right;
+	return left;
+}
+
 // ---------------------------------------------------------------------------------------------------------------------
 // Class Node
 // ---------------------------------------------------------------------------------------------------------------------
@@ -45,6 +70,12 @@ void algorithms::graphs::Node::setPosition(const NodeCoord & coord)
 {
 	_x = coord.first;
 	_y = coord.second;
+}
+
+void algorithms::graphs::Node::move(const NodeForce & force)
+{
+	_x += force.first;
+	_y += force.second;
 }
 
 std::ostream& algorithms::graphs::operator <<(std::ostream & out, const Node & node)
@@ -98,7 +129,7 @@ void Graph::swap(Graph & other)
 {
 	using std::swap;
 	swap(_nodes, other._nodes);
-	swap(_edges, other._edges);
+	swap(_edges, other._edges);	
 }
 
 Graph & algorithms::graphs::Graph::operator=(Graph other)
@@ -135,43 +166,68 @@ void Graph::_initPositionNodes()
 }
 
 void algorithms::graphs::Graph::calcForces()
-{
-	std::vector<NodeCoord> forces(_nodes.size());
-	// Calculate repel forces	
-	for (size_t i = 0; i < _nodes.size() - 1; ++i)
+{	
+	std::vector<NodeForce> cachedForce(_nodes.size());
+	bool stopIterating = false;
+	while (!stopIterating)
 	{
-		for (size_t j = i + 1; j < _nodes.size(); ++j)
+		std::vector<NodeForce> forces(_nodes.size());
+		// Calculate repel forces	
+		for (size_t i = 0; i < _nodes.size() - 1; ++i)
 		{
-			auto iPos = _nodes[i].getPosition();
-			auto jPos = _nodes[j].getPosition();
-			auto theta = atan2(jPos.second - iPos.second, jPos.first - iPos.first);
-			auto rForce = repelForce(iPos, jPos);
-			forces[i].first -= (rForce * cos(theta));
-			forces[i].second -= (rForce * sin(theta));
-			forces[j].first += (rForce * cos(theta));
-			forces[j].second += (rForce * sin(theta));
+			for (size_t j = i + 1; j < _nodes.size(); ++j)
+			{
+				auto iPos = _nodes[i].getPosition();
+				auto jPos = _nodes[j].getPosition();
+				auto theta = atan2(jPos.second - iPos.second, jPos.first - iPos.first);
+				auto rForce = repelForce(iPos, jPos);
+				forces[i].first -= (rForce * cos(theta));
+				forces[i].second -= (rForce * sin(theta));
+				forces[j].first += (rForce * cos(theta));
+				forces[j].second += (rForce * sin(theta));
+			}
 		}
-	}
-	
-	// Calculate attractive forces
-	for (size_t i = 0; i < _edges.size(); ++i)
-	{		
-		auto node0Pos = _nodes[_edges[i].start()].getPosition();
-		auto node1Pos = _nodes[_edges[i].end()].getPosition();
-		auto aForce = attractForce(node0Pos, node1Pos);
-		auto theta = atan2(node1Pos.second - node0Pos.second, node1Pos.first - node0Pos.first);
-		forces[_edges[i].start()].first += (aForce * cos(theta));
-		forces[_edges[i].start()].second += (aForce * sin(theta));
-		forces[_edges[i].end()].first -= (aForce * cos(theta));
-		forces[_edges[i].end()].second -= (aForce * sin(theta));
-	}
 
-	// Update positions
-	for (size_t i = 0; i < _nodes.size(); ++i)
-	{
-		const auto& currentPos = _nodes[i].getPosition();
-		const auto& delta = forces[i];
-		NodeCoord newPos{ currentPos.first + delta.first, currentPos.second + delta.second };
-		_nodes[i].setPosition(newPos);
-	}	
+		// Calculate attractive forces
+		for (size_t i = 0; i < _edges.size(); ++i)
+		{
+			auto node0Pos = _nodes[_edges[i].start()].getPosition();
+			auto node1Pos = _nodes[_edges[i].end()].getPosition();
+			auto aForce = attractForce(node0Pos, node1Pos);
+			auto theta = atan2(node1Pos.second - node0Pos.second, node1Pos.first - node0Pos.first);
+			forces[_edges[i].start()] += { (aForce * cos(theta)) , (aForce * sin(theta)) };
+			forces[_edges[i].end()] -= { (aForce * cos(theta)), (aForce * sin(theta)) };
+		}
+		
+		// Update positions
+		std::transform(_nodes.begin(), _nodes.end(), forces.begin(), _nodes.begin(),
+			[](Node& c1, const NodeForce& c2)
+		{
+			c1.move(c2);
+			return c1;
+		});
+		
+		// Calculate the net forces to test if we need to keep iterating.
+		std::vector<NodeForce> diffForces(_nodes.size());
+		std::transform(
+			forces.begin(),
+			forces.end(),
+			cachedForce.begin(),
+			diffForces.begin(),
+			[](const NodeForce& c1, const NodeForce& c2)
+		{
+			return c1 - c2;
+		});
+
+		// check if nodes are stable.
+		stopIterating = std::all_of(
+			diffForces.begin(),
+			diffForces.end(),
+			[](const NodeForce& nf) 
+		{
+			return nf.first < constants::threshold && nf.second < constants::threshold;
+		});
+
+		cachedForce = forces;
+	}
 }
